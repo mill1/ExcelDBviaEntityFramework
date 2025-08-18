84efa38f1fa207f3f64f731ca8249c5afcecb125
Emiel Nijhuis <emiel.nijhuis@speerit.nl>
2025-08-18 10:44:37 +0200

    Edited Excel file

--- ExcelDBviaEntityFramework/Constants.cs
+++ ExcelDBviaEntityFramework/Constants.cs
@@ -3,7 +3,7 @@
     public static class Constants
     {
         // 'Conventions'
-        public const string ExcelFileName = "Signups.xlsx";
+        public const string ExcelFileName = "Signups_v2.xlsx";
         public const string SheetNameSignups = "Signups$";
         public const int ColumnIndexDeleted = 1;
         public const int ColumnIndexId = 2;
